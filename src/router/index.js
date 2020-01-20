5986e768a5158a4f87ce833e3f078eb3320abad1
TiagoNandes <tiagonandes@hotmail.com>
2020-01-20 15:47:39 +0000

    routing updated

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Main from '../views/Main.vue'
+import AddFunds from '../views/AddFunds.vue'
+import WeeklyMenu from '../views/WeeklyMenu.vue'
+import Reservation from '../views/Reservation.vue'
+import Confirmation from '../views/Confirmation.vue'
+import Restaurant from '../views/Restaurant.vue'
+
 import store from '../store'
 import Login from '../views/auth/Login.vue'
 import Register from '../views/auth/Register.vue'
@@ -9,7 +16,7 @@ import usersList from '../views/usersList.vue'
 //import homeAdmin from '../views/admin/homeAdmin.vue'
 import menuOfTheWeek from '../views/menuOfTheWeek.vue'
 import reserva from '../views/reserva.vue'
-import feedback from '../views/Feedback.vue'
+import Feedback from '../views/Feedback.vue'
 import menuSelection from '../views/menuSelection.vue'
 import addEmentaSemanal from '../views/admin/addEmentaSemanal.vue'
 import addNewUser from '../views/admin/addNewUser.vue'
@@ -22,8 +29,7 @@ import statiscsDay from '../views/admin/statisticsDay.vue'
 import statisticsFeedbacks from '../views/admin/statisticsFeedbacks.vue'
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'home',
     component: Home
@@ -37,82 +43,114 @@ const routes = [
     path: '/register',
     name: 'register',
     component: Register
-  },{
-    path:'/usersList',
-    name:'usersList',
-    component:usersList
-  },/*{
-    path:'/homeAdmin',
-    name:'homeAdmin',
-    component:homeAdmin
-  },*/
+  }, {
+    path: '/usersList',
+    name: 'usersList',
+    component: usersList
+  },
+  /*{
+      path:'/homeAdmin',
+      name:'homeAdmin',
+      component:homeAdmin
+    },*/
   {
-    path:'/menuOfTheWeek',
-    name:'menuOfTheWeek',
-    component:menuOfTheWeek
-  },{
-    path:'/reserva',
-    name:'reserva',
-    component:reserva
+    path: '/menuOfTheWeek',
+    name: 'menuOfTheWeek',
+    component: menuOfTheWeek
+  }, {
+    path: '/reserva',
+    name: 'reserva',
+    component: reserva
   },
   {
-    path:'/feedback',
-    name:'feedback',
-    component:feedback
+    path: '/feedback',
+    name: 'feedback',
+    component: Feedback
   },
   {
-    path:'/menuSelection',
-    name:'menuSelection',
-    component:menuSelection
+    path: '/menuSelection',
+    name: 'menuSelection',
+    component: menuSelection
   },
-{
-  path:'/addEmentaSemanal',
-  name:'addEmentaSemanal',
-  component:addEmentaSemanal
-},
-{
-  path:'/addNewUser',
-  name:'addNewUser',
-  component: addNewUser
-},
-{
-  path:'/listAllUsers',
-  name:'listAllUsers',
-  component: listAllUsers
-},
-{
-  path:'/listAllEmentas',
-  name:'listAllEmentas',
-  component: listAllEmentas
-},
-{
-  path:'/listAllReservations',
-  name:'listAllReservations',
-  component: listAllReservations
-},
-{
-  path:'/listAllFeedbacks',
-  name:'listAllFeedbacks',
-  component: listAllFeedbacks
-},{
-  path:'/editUser',
-  name:'editUser',
-  component:editUser
-},
-{
-  path:'/About',
-  name:'About',
-  component:About
-}, 
-{
-  path:'/statisticsDay',
-  name:'statisticsDay',
-  component:statiscsDay
-}, {
-  path:'/statisticsFeedbacks',
-  name:'statisticsFeedbacks',
-  component:statisticsFeedbacks
-}
+  {
+    path: '/addEmentaSemanal',
+    name: 'addEmentaSemanal',
+    component: addEmentaSemanal
+  },
+  {
+    path: '/addNewUser',
+    name: 'addNewUser',
+    component: addNewUser
+  },
+  {
+    path: '/listAllUsers',
+    name: 'listAllUsers',
+    component: listAllUsers
+  },
+  {
+    path: '/listAllEmentas',
+    name: 'listAllEmentas',
+    component: listAllEmentas
+  },
+  {
+    path: '/listAllReservations',
+    name: 'listAllReservations',
+    component: listAllReservations
+  },
+  {
+    path: '/listAllFeedbacks',
+    name: 'listAllFeedbacks',
+    component: listAllFeedbacks
+  },
+  {
+    path: '/editUser',
+    name: 'editUser',
+    component: editUser
+  },
+  {
+    path: '/About',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/main',
+    name: 'main',
+    component: Main
+  },
+  {
+    path: '/addFunds',
+    name: 'addFunds',
+    component: AddFunds
+  },
+  {
+    path: '/weeklyMenu',
+    name: 'weeklyMenu',
+    component: WeeklyMenu
+  },
+  {
+    path: '/reservation',
+    name: 'reservation',
+    component: Reservation
+  },
+  {
+    path: '/confirmation',
+    name: 'confirmation',
+    component: Confirmation
+  },
+  {
+    path: '/restaurant',
+    name: 'restaurant',
+    component: Restaurant
+  },
+  {
+    path: '/statisticsDay',
+    name: 'statisticsDay',
+    component: statiscsDay
+  }, {
+    path: '/statisticsFeedbacks',
+    name: 'statisticsFeedbacks',
+    component: statisticsFeedbacks
+  }
 
 ]
 
