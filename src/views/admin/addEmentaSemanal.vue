18e0ce20255c7c805e82c315be468c10843a4e30
TiagoNandes <tiagonandes@hotmail.com>
2020-01-30 19:08:13 +0000

    addEmentaSemanal updated

--- src/views/admin/addEmentaSemanal.vue
+++ src/views/admin/addEmentaSemanal.vue
@@ -14,7 +14,7 @@
           </div>
           <div class="col">
             <div class="form-group">
-              <label  required for="sel1">Selecione o horário:</label>
+              <label required for="sel1">Selecione o horário:</label>
               <select class="form-control" id="sel1" name="sellist1" v-model="mealTime">
                 <option>Almoço</option>
                 <option>Jantar</option>
@@ -22,17 +22,17 @@
             </div>
           </div>
           <div class="col">
-              <label for="availableSeats">Lugares disponíveis: </label>
-              <input
+            <label for="availableSeats">Lugares disponíveis:</label>
+            <input
               minlength="1"
-                min="1"
-                required
-                type="number"
-                class="form-control"
-                id="availableSeats"
-                v-model="availableSeats"
-                placeholder="ex: 150"
-              />
+              min="1"
+              required
+              type="number"
+              class="form-control"
+              id="availableSeats"
+              v-model="availableSeats"
+              placeholder="ex: 150"
+            />
           </div>
         </div>
         <!-- ver https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input/data para mais info -->
@@ -42,7 +42,7 @@
           <div class="col">
             <label for="entrada" class="mr-sm-2">Entrada:</label>
             <input
-            minlength="3"
+              minlength="3"
               type="text"
               class="form-control mb-2 mr-sm-2"
               id="entrada"
@@ -57,7 +57,7 @@
           <div class="col">
             <label for="sopa" class="mr-sm-2">Sopa:</label>
             <input
-            minlength="3"
+              minlength="3"
               type="text"
               class="form-control mb-2 mr-sm-2"
               id="sopa"
@@ -75,7 +75,7 @@
           <div class="col">
             <label for="pratoCarne">Prato Carne:</label>
             <input
-            minlength="3"
+              minlength="3"
               type="text"
               class="form-control"
               id="pratoCarne"
@@ -90,7 +90,7 @@
           <div class="col">
             <label for="pratoPeixe">Prato Peixe:</label>
             <input
-            minlength="3"
+              minlength="3"
               type="text"
               class="form-control"
               id="pratoPeixe"
@@ -108,7 +108,7 @@
           <div class="col">
             <label for="pratoVeg">Prato Vegetariano:</label>
             <input
-            minlength="3"
+              minlength="3"
               type="text"
               class="form-control"
               id="pratoVeg"
@@ -124,7 +124,7 @@
           <div class="col">
             <label for="sobremesa">Sobremesa:</label>
             <input
-            minlength="3"
+              minlength="3"
               required
               type="text"
               class="form-control"
@@ -145,6 +145,7 @@
 </template>
 <script>
 import { mapGetters, mapMutations } from "vuex";
+import Swal from "sweetalert2";
 
 import Navbar from "@/components/homeAdmin.vue";
 export default {
@@ -184,26 +185,57 @@ export default {
         pratoVeg: this.pratoVeg,
         sobremesa: this.sobremesa
       };
-      if (this.getIdMenuByDaySchedule(newMeal.mealTime, newMeal.date)) {
-        //create meals
-        let menu = this.getIdMenuByDaySchedule(newMeal.mealTime, newMeal.date)
-          .idMenu;
-        this.mealCreater([menu, newMeal.sopa, 1]);
-        this.mealCreater([menu, newMeal.entrada, 2]);
-        this.mealCreater([menu, newMeal.pratoCarne, 3]);
-        this.mealCreater([menu, newMeal.pratoPeixe, 4]);
-        this.mealCreater([menu, newMeal.pratoVeg, 5]);
-        this.mealCreater([menu, newMeal.sobremesa, 6]);
+      if (
+        this.date === "" ||
+        this.mealTime === "" ||
+        this.availableSeats === "" ||
+        this.sopa === "" ||
+        this.entrada === "" ||
+        this.pratoCarne === "" ||
+        this.pratoPeixe === "" ||
+        this.pratoVeg === "" ||
+        this.sobremesa === ""
+      ) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Preencha todos os campos!",
+          confirmButtonColor: "#127834"
+        });
       } else {
-        //create menu then create meals
-        this.menuCreater([newMeal.date, newMeal.mealTime, newMeal.availableSeats]);
-        let menu = this.getIdMenuByDaySchedule(newMeal.mealTime, newMeal.date);
-        this.mealCreater([menu.idMenu, newMeal.sopa, 1]);
-        this.mealCreater([menu.idMenu, newMeal.entrada, 2]);
-        this.mealCreater([menu.idMenu, newMeal.pratoCarne, 3]);
-        this.mealCreater([menu.idMenu, newMeal.pratoPeixe, 4]);
-        this.mealCreater([menu.idMenu, newMeal.pratoVeg, 5]);
-        this.mealCreater([menu.idMenu, newMeal.sobremesa, 6]);
+        if (this.getIdMenuByDaySchedule(newMeal.mealTime, newMeal.date)) {
+          //create meals
+          let menu = this.getIdMenuByDaySchedule(newMeal.mealTime, newMeal.date)
+            .idMenu;
+          this.mealCreater([menu, newMeal.sopa, 1]);
+          this.mealCreater([menu, newMeal.entrada, 2]);
+          this.mealCreater([menu, newMeal.pratoCarne, 3]);
+          this.mealCreater([menu, newMeal.pratoPeixe, 4]);
+          this.mealCreater([menu, newMeal.pratoVeg, 5]);
+          this.mealCreater([menu, newMeal.sobremesa, 6]);
+        } else {
+          //create menu then create meals
+          this.menuCreater([
+            newMeal.date,
+            newMeal.mealTime,
+            newMeal.availableSeats
+          ]);
+          let menu = this.getIdMenuByDaySchedule(
+            newMeal.mealTime,
+            newMeal.date
+          );
+          this.mealCreater([menu.idMenu, newMeal.sopa, 1]);
+          this.mealCreater([menu.idMenu, newMeal.entrada, 2]);
+          this.mealCreater([menu.idMenu, newMeal.pratoCarne, 3]);
+          this.mealCreater([menu.idMenu, newMeal.pratoPeixe, 4]);
+          this.mealCreater([menu.idMenu, newMeal.pratoVeg, 5]);
+          this.mealCreater([menu.idMenu, newMeal.sobremesa, 6]);
+        }
+        Swal.fire({
+          text: "Ementa semanal adicionada!",
+          icon: "success",
+          confirmButtonColor: "#127834"
+        });
       }
     }
   }
